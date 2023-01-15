567b77ef9c0c132f7717d85dc82359dd38031234
LMG <trintsa@mail.ru>
2023-01-15 08:24:12 +0500

    delete media

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Profession
 
-# Register your models here.
 
-admin.site.register(Profession)
+# Register your models here.
+class ProfessionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'images')
+    search_fields = ('name', )
+
+
+admin.site.register(Profession, ProfessionAdmin)
--- base/models.py
+++ base/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Profession(models.Model):
-    name = models.CharField(max_length=64)
-    description = models.TextField(blank=True)
-    images = models.ImageField(upload_to='photo_prof/')
+    name = models.CharField(max_length=64, verbose_name='Название профессии')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    images = models.ImageField(upload_to='photo_prof/', verbose_name='Фото')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Профессия"
+        verbose_name_plural = "Профессии"
+        ordering = ['name']
